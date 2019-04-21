efbaf0903bb35d383b2e67bce7a6df1d5c1860a2
Janice Pena <jipena94@gmail.com>
2019-04-21 03:24:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   elsif array.length == 3
   array[0..-2].join( ", ") + ", and starfruit"
   else array.length > 3
-    array[-1].prepend( " and ")
+    array[-1].prepend( "and ")
   end
 end
  
