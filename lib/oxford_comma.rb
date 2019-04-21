9b751be9733052fc239f8897079e5c765c5e1695
Janice Pena <jipena94@gmail.com>
2019-04-21 03:25:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array[-2] << " and "
+    array.join
+  elsif array.length == 3
+  array[0..-2].join( ", ") + ", and starfruit"
+  else array.length > 3
+    array[-1].prepend( "and ")
+    array.join(", ")
+  end
 end
+ 
