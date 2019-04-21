bd8e10431c4590488d0add08397a7f7769607d83
Janice Pena <jipena94@gmail.com>
2019-04-21 03:07:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array[-2] << " and "
+    array.join
+  elsif array.length == 3
+  array[0..-1].join( ", ") + " and "
+  else array.length > 3
+  end
 end
+ 
